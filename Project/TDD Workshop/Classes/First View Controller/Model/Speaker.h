79cba7c5b396e32d4031f08d756d7c47f9161850
Pawel Dudek <pawel@dudek.mobi>
2014-04-20 21:35:16 +0200

    Reorganized code structure a bit

---
+++ Project/TDD Workshop/Classes/First View Controller/Model/Speaker.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2014 Taptera Inc. All rights reserved.
+ */
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface Speaker : NSObject
+@property(nonatomic, readonly) NSString *name;
+@property(nonatomic, readonly) UIImage *photo;
+
+@end
