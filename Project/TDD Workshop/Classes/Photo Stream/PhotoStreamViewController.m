5c177fe307714ca8c0b488f66525b870fd8b73f2
Aleksander Zubala <alek.zubala@gmail.com>
2014-04-22 23:01:09 +0200

    Removed test code.

--- Project/TDD Workshop/Classes/Photo Stream/PhotoStreamViewController.m
+++ Project/TDD Workshop/Classes/Photo Stream/PhotoStreamViewController.m
@@ -37,11 +37,6 @@ NSString * const PhotoStreamViewControllerCellId = @"PhotoStreamViewControllerCe
     [super loadView];
     [self setupCollectionView];
     [self setupRefreshControl];
-
-    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
-    testObject[@"foo"] = @"bar";
-    [testObject saveInBackground];
-
 }
 
 - (void)setupCollectionView {
