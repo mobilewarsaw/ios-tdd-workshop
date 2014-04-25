54486787b49ede383ae25ef0004efd467822a9ec
Aleksander Zubala <az@taptera.com>
2014-04-26 01:06:13 +0200

    StreamItemPreviewViewControllerSpec has basic setup.

--- Project/TDD Workshop/Classes/Photo Stream/Preview/StreamItemPreviewViewControllerSpec.m
+++ Project/TDD Workshop/Classes/Photo Stream/Preview/StreamItemPreviewViewControllerSpec.m
@@ -1,7 +1,17 @@
 #import "Specs.h"
+#import "StreamItemPreviewViewController.h"
 
 SPEC_BEGIN(StreamItemPreviewViewControllerSpec)
 describe(@"StreamItemPreviewViewController", ^{
 
+    __block StreamItemPreviewViewController *streamItemPreviewViewController;
+
+    beforeEach(^{
+        streamItemPreviewViewController = [StreamItemPreviewViewController new];
+    });
+
+    afterEach(^{
+        streamItemPreviewViewController = nil;
+    });
 });
 SPEC_END
